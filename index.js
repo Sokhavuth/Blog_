c2bc0fb1ecc83a90d043fd4dd7eb7910ad8c0c93
Sokhavuth TIN <vuthdevelop@gmail.com>
2022-04-08 20:29:09 +0700

    Make it better

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 //index.js
 //npm install express
 //npm install ejs
+//npm install express-session
+//npm install connect-mongo
 
 import express from 'express'
 import path from 'path'
+import dotenv from 'dotenv'
+dotenv.config()
+import session from 'express-session'
+import MongoStore from 'connect-mongo' 
 
 const app = express()
+
+app.use(session({
+    srote: MongoStore.create({mongoUrl: process.env.DATABASE_URI}),
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 2 // two weeks
+    }
+}))
+
 const port = process.env.PORT || 3000
 process.env.TZ = "Asia/Phnom_Penh"
 const __dirname = path.resolve()
- 
+
 import index from './routes/index.js'
 import admin from './routes/admin.js'
+import mydb from './models/conMongoDB.js'
+
+app.use('/',async function(req,res,next){
+    req.mydb = await mydb
+    req.__dirname = __dirname
+    next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
