b8d05e330206fc9ba68b87b11d80416b0608223f
Sokhavuth TIN <vuthdevelop@gmail.com>
2022-04-09 11:48:03 +0700

    Make it better

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 //routes/index.js
 import express from 'express'
 const index = express.Router()
+import settings from '../settings.js'
 
 index.get('/',(req,res)=>{
-    res.render('base')
+    res.render('base',settings)
 })
  
 export default index
