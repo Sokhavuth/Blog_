acbddc0d720ad5dbf02a904a6b31021f34098eb3
Sokhavuth <vuthdevelop@gmail.com>
2022-05-21 13:09:27 +0700

    Make it better

--- controllers/front/index.js
+++ controllers/front/index.js
@@ -10,7 +10,7 @@ export default async (req,res)=>{
     mySettings.route = '/'
     mySettings.type = 'book'
 
-    mySettings.randomArticles = await randomArticles(req,10)
+    mySettings.randomArticles = await randomArticles(req,15)
     mySettings.randomBooks = await randomBooks(req,8)
     mySettings.randomVideos = await randomVideos(req,13)
     
--- models/book/randomArticles.js
+++ models/book/randomArticles.js
@@ -1,6 +1,6 @@
 //models/post/random.js
 
-export default async (req,amount=10)=>{
+export default async (req,amount=15)=>{
     const pipeline = [{$sample:{size: amount}}]
     return await req.mydb.collection("books").aggregate(pipeline).toArray()
 }
