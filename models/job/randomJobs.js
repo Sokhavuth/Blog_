24c51b8445611a82aad7ce5f038333f3a6ee34b3
Sokhavuth <vuthdevelop@gmail.com>
2022-05-22 16:44:01 +0700

    Make it better

--- models/job/randomJobs.js
+++ models/job/randomJobs.js
@@ -1,6 +1,12 @@
 //models/job/randomJobs.js
 
 export default async (req,amount=15)=>{
-    const pipeline = [{$sample:{size:amount}}]
+    let pipeline = null
+    if(req.params.id){
+        pipeline = [{$match:{id:{$not:{$eq:req.params.id}}}},{$sample:{size:amount}}]
+    }else{
+        pipeline = [{$sample:{size:amount}}]
+    }
+    
     return await req.mydb2.collection("jobs").aggregate(pipeline).toArray()
 }
