e4d6adf52d2810ddcd52e27837e6a51bf55d45e9
Sokhavuth <vuthdevelop@gmail.com>
2022-05-26 16:11:35 +0700

    Make it better

--- models/book/getArticles.js
+++ models/book/getArticles.js
@@ -2,5 +2,5 @@
 
 export default async (req,bookTitle)=>{
     const pipeline = [{$match: {bookTitle: bookTitle}}]
-    return await req.mydb.collection('books').aggregate(pipeline).toArray()
+    return await req.mydb.collection('books').aggregate(pipeline).sort({date:1,_id:1}).toArray()
 }
